dd2d43669b6761492f61331f3f240f330afdb174
Dahesor <dahesor@gmail.com>
2025-12-19 06:57:07 -0700

    Change Build Logic

--- scripts/reset.py
+++ scripts/reset.py
@@ -3,33 +3,34 @@ import random
 import shutil
 import os
 from reset_.pathes import get_pathes
-from reload import copy_datapack
 
-
-def reset_world(level_dat_path, backup_path, target_folder):
-    # 备份原始文件
-    shutil.copyfile(level_dat_path, backup_path)
-    print(f'✅ 原始文件已备份为: {backup_path}')
+def reset_world(world_folder, data_pack_folder, source_folder):
+    assert 'level.dat' in os.listdir(world_folder)
     # 加载 NBT 数据
-    nbt_file = nbtlib.load(level_dat_path)
+    level_data_path = os.path.join(world_folder, 'level.dat')
+    nbt_file = nbtlib.load(level_data_path)
     data = nbt_file['Data']  # 修改此行
 
     # 生成新种子
     new_seed = random.randint(-2**63, 2**63 - 1)
     data['WorldGenSettings']['seed'] = nbtlib.tag.Long(new_seed)
+    data['DayTime'] = nbtlib.tag.Long(2000)
     print(f'🆕 替换为新种子: {new_seed}')
-
     # 删除玩家数据
-    del data['Player']
-
+    try:
+        del data['Player']
+    except KeyError:
+        pass
     # 保存修改
-    nbt_file.save(level_dat_path)
+    nbt_file.save(level_data_path)
     print('💾 新的 level.dat 已保存！')
     # 遍历文件夹
-    for item in os.listdir(target_folder):
-        full_path = os.path.join(target_folder, item)
-        # 如果不是 level.dat，就删除
-        if item != 'level.dat':
+    for item in os.listdir(world_folder):
+    # 白名单
+        if item in ('level.dat', 'datapacks'):
+            continue
+        full_path = os.path.join(world_folder, item)
+        if item in ('advancements','data','DIM1','DIM-1','stats','entities','playerdata','poi','region','session.lock'):
             try:
                 if os.path.isfile(full_path) or os.path.islink(full_path):
                     os.remove(full_path)
@@ -39,6 +40,5 @@ def reset_world(level_dat_path, backup_path, target_folder):
                 print(f'⚠️ 删除失败: {full_path}，错误: {e}')
 
 if __name__ == "__main__":
-    (level_dat_path, backup_path, target_folder, data_pack_folder, source_folder) = get_pathes()
-    reset_world(level_dat_path=level_dat_path, backup_path=backup_path, target_folder=target_folder)
-    copy_datapack(data_pack_folder=data_pack_folder, source_folder=source_folder)
+    (world_folder, data_pack_folder, source_folder) = get_pathes()
+    reset_world(world_folder, data_pack_folder, source_folder)
--- scripts/reset_/pathes.py
+++ scripts/reset_/pathes.py
@@ -1,7 +1,8 @@
+from pathlib import Path
+
 def get_pathes():
-    level_dat_path = 'C:/Users/dahes/AppData/Roaming/.minecraft/versions/Survival Competiton/saves/mod/level.dat'  # 原始文件
-    backup_path = 'C:/Users/dahes/AppData/Roaming/.minecraft/versions/Survival Competiton/saves/mod/level.dat_old'  # 备份文件
-    target_folder = 'C:/Users/dahes/AppData/Roaming/.minecraft/versions/Survival Competiton/saves/mod/'
-    data_pack_folder = 'C:/Users/dahes/AppData/Roaming/.minecraft/versions/Survival Competiton/saves/mod/datapacks/Survival Competiton'
-    source_folder = '.'
-    return (level_dat_path, backup_path, target_folder, data_pack_folder, source_folder)
+    world_folder = (Path.cwd() / "../..").resolve()
+    data_pack_folder = (Path.cwd() / "../").resolve()
+    source_folder = (Path.cwd() / ".").resolve()
+    print(str(world_folder))
+    return (str(world_folder), str(data_pack_folder), str(source_folder))
